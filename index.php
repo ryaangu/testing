cfe886acf1ab0179054d62dbfba3cd01ffab2b4c
Ryan <47481741+ryaangu@users.noreply.github.com>
2021-01-27 11:44:30 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+    // #Get a secure filename
+    // $username = "myUsername"
+    // $filename = uniqid($username, true).".txt";
+
+    // //Create the array
+    // $userInfo = ["username" => $username, "points" => 0];
+
+    // // Store the data
+    // file_put_contents($filename, serialize($userInfo));
+
+    // // How to get the data
+    // $userInfo = unserialize(file_get_contents($filename));
+    // print($userInfo["points"]);
+    // //or
+    // print($userInfo["username"]);
+    echo "hello";
+
+?>
